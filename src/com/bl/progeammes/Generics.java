3c5e7efeb6b0974ce7eede69dd0f5c50ec0ee8a9
anzar Shaikh <anzarshaikh373@gmail.com>
2021-07-06 20:27:37 +0530

    the uc4 is done is more than three parameters are added

--- src/com/bl/progeammes/Generics.java
+++ src/com/bl/progeammes/Generics.java
@@ -2,23 +2,26 @@ package com.bl.progeammes;
 
 public class Generics<T extends Comparable<T>> {
 
-    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3) {
+    public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
         T max = val1;
         if (val2.compareTo(max) > 0 ) {
             max = val2;
         } if (val3.compareTo(max) > 0){
             max = val3;
-        }
-        System.out.println("maximum value is " + max);
-        return max;
+        }if (val4.compareTo(max) > 0 ){
+            max = val4;
+        }if (val5.compareTo(max) > 0){
+            max = val5;
 
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         System.out.println("*******welcome to generics code tested By JUnit***** ");
 
         Generics generics = new Generics();
-        generics.maximumValue(4.5f,5.6f,7.2f);
+        System.out.println( "The maximum value is :: "+maximumValue(2.5f,3.5f,4.5f,5.5f,6.5f));
 
 
 
