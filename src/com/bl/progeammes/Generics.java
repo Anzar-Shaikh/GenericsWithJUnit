cec662dbcca3e2a4f34cf57e3fefe45c9bb69ff3
anzar Shaikh <anzarshaikh373@gmail.com>
2021-07-06 20:09:26 +0530

    the class made generic as per refactor2

--- src/com/bl/progeammes/Generics.java
+++ src/com/bl/progeammes/Generics.java
@@ -14,8 +14,6 @@ public class Generics<T extends Comparable<T>> {
 
     }
 
-
-
     public static void main(String[] args) {
         System.out.println("*******welcome to generics code tested By JUnit***** ");
 
