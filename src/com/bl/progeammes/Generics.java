18362af4296efcb73237bf3789a46771fa71146b
anzar Shaikh <anzarshaikh373@gmail.com>
2021-07-06 18:53:08 +0530

    welcome to *******welcome to generics code tested By JUnit*****

---
+++ src/com/bl/progeammes/Generics.java
@@ -0,0 +1,10 @@
+package com.bl.progeammes;
+
+public class Generics {
+
+
+    public static void main(String[] args) {
+        System.out.println("*******welcome to generics code tested By JUnit***** ");
+
+    }
+}
